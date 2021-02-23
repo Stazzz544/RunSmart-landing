eb0ccc75fd52c012f8dc61ef8b0f859c2db6ac1a
Stas_PC <stazzzdzr@gmail.com>
2021-02-23 20:32:14 +0300

    2 section ready

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,14 @@ let path = {
 		html: [source_folder + "/*.html", "!"+source_folder + "/_*.html"],//исключаем все файлы начинающиеся с символа подчёркивание
 		css: source_folder + "/scss/style.scss",
 		js: source_folder + "/js/script.js",
-		img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",  //две звёздочки означает, что мы будем слушать все подпапки в папке src/img, одна звёздочка - любое название
+		img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",  //две звёздочки означает, что мы будем слушать все подпапки в папке src/img, одна звёздочка - любое название
 		fonts: source_folder + "/fonts/*.ttf",
 	},
 	watch: {  //указываем что мы будем слушать
 		html: source_folder + "/**/*.html",
 		css: source_folder + "/scss/**/*.scss",
 		js: source_folder + "/js/**/*.js",
-		img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
+		img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
 	},
 	clean: "./" + project_folder + "/" //объект который удаляет папку готового проета при кардом запуске галпа
 }
